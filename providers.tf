a8832eacd29ab75a17481a58877f8e7bc7726920
Md Azhar <md.azhar@sumologic.com>
2026-07-29 18:17:15 +0530

    fixed base url condition

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ provider "sumologic" {
   access_id   = var.sumologic_access_id
   access_key  = var.sumologic_access_key
   base_url    = var.sumologic_environment_base_url
-  environment = var.sumologic_environment_base_url == null ? var.sumologic_environment : null
+  environment = (var.sumologic_environment_base_url == null || var.sumologic_environment_base_url == "") ? var.sumologic_environment : null
 }
 
 provider "aws" {
